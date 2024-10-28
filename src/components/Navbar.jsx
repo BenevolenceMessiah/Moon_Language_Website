72dde874a2f2c2c5efac8eb25945ae56f6aeb105
BenevolenceMessiah <benevolence.messiah@gmail.com>
2024-10-28 19:23:33 -0400

    .jsk nav zed axis management.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="...">
+    <nav className="fixed top-0 left-0 right-0 bg-[#1f1f1f] shadow-lg z-40">
       {/* ... existing nav content ... */}
       
       {/* Make sure the hamburger button has an onClick handler */}
@@ -22,9 +22,8 @@ function Navbar() {
         </svg>
       </button>
 
-      {/* Add conditional rendering for mobile menu */}
-      <div className={`lg:hidden ${isOpen ? 'block' : 'hidden'} absolute top-full left-0 right-0 bg-[#1f1f1f]`}>
-        {/* Mobile menu items */}
+      {/* Mobile menu */}
+      <div className={`lg:hidden ${isOpen ? 'block' : 'hidden'} absolute top-full left-0 right-0 bg-[#1f1f1f] z-50 w-full`}>
         <div className="px-2 pt-2 pb-3 space-y-1">
           {/* ... menu items ... */}
         </div>
